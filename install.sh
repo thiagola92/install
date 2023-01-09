c08e336158955278d168e2d548756ca51140f80b
Thiago Lages de Alencar <thiagola92@gmail.com>
2023-01-09 15:33:06 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -137,9 +137,9 @@ sudo nala install -y xclip;
 ######################################################
 
 # browser
-sudo nala install -y apt-transport-https curl gnupg;
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo nala-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -;
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list;
+sudo nala install -y curl;
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg;
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list;
 sudo nala update;
 sudo nala install -y brave-browser;
 
