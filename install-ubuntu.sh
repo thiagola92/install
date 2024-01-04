fd42d8927af64b8f45b5bd139802d457dea8ea22
Thiago Lages de Alencar <thiagola92@gmail.com>
2024-01-03 23:02:43 -0300

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -314,7 +314,9 @@ eval \"\$(pyenv virtualenv-init -)\"" | tee -a ~/.bash_profile ~/.bashrc;
 # pdm
 sudo nala install -y python3-venv;
 curl -sSL https://pdm-project.org/install-pdm.py | python3 -;
-export PATH=~/.local/bin:$PATH;
+echo "
+# pdm
+export PATH=~/.local/bin:\$PATH" | tee -a ~/.bash_profile ~/.bashrc;
 source ~/.bashrc;
 pdm completion bash | sudo tee /etc/bash_completion.d/pdm.bash-completion;
 
