13b500e1fed9fcf59a933f5a5e9e59f614140ad1
Thiago Lages de Alencar <thiagola92@gmail.com>
2024-04-07 19:38:13 -0300

    Update export_bookmarks.sh

--- bookmarks/export_bookmarks.sh
+++ bookmarks/export_bookmarks.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-git clone https://gist.github.com/d5e13230686ade1a598fa65b2dee9668.git repo
-cd repo
+cd /tmp
+git clone https://gist.github.com/d5e13230686ade1a598fa65b2dee9668.git tmp_repo
+cd tmp_repo
 cp ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks Bookmarks
 git commit -am "Backup"
 git push
 cd ..
-rm -rf repo
+rm -rf tmp_repo
