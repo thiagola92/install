5e9af32f8bea35c278499f667a140d07b32b6bd6
Thiago Lages de Alencar <thiagola92@gmail.com>
2024-06-24 21:05:29 -0300

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -187,12 +187,11 @@ sudo nala install -y usb-creator-gtk;
 sudo flatpak install -y flathub ca.desrt.dconf-editor;
 
 # browser
+# netflix: activate "Widevine", force update in brave://components/ and restart browser
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg;
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list;
 sudo nala update;
 sudo nala install -y brave-browser;
-# You may have to manually activate "Widevine"
-# Force update in brave://components/
 
 # discord
 sudo flatpak install -y flathub com.discordapp.Discord;
