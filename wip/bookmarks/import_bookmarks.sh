40dcb36c7bfd67dbed58f32b8c4d6c9e857b3f80
Thiago Lages de Alencar <thiagola92@gmail.com>
2024-10-13 01:30:11 -0300

    Update import_bookmarks.sh

--- wip/bookmarks/import_bookmarks.sh
+++ wip/bookmarks/import_bookmarks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /tmp
-git clone https://gist.github.com/d5e13230686ade1a598fa65b2dee9668.git tmp_repo
+git clone https://github.com/thiagola92/bookmarks.gitt tmp_repo
 cp repo/Bookmarks ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks
 rm -rf tmp_repo
