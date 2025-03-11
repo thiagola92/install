0eb8ee725c909683e978047a1d411c11c79b54bb
Thiago Lages de Alencar <thiagola92@gmail.com>
2025-03-11 09:45:05 -0300

    Update import_bookmarks.sh

--- ubuntu/import_bookmarks.sh
+++ ubuntu/import_bookmarks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Use this if Brave import stop working.
 cd /tmp
 git clone https://github.com/thiagola92/bookmarks.git tmp_repo
 cp tmp_repo/Bookmarks ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks
