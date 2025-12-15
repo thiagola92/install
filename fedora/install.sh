30eb835adda1018f7d5857ade7fc42314d96e425
Thiago Lages de Alencar <thiagola92@gmail.com>
2025-12-15 18:40:58 -0300

    Start docker daemon

--- fedora/install.sh
+++ fedora/install.sh
@@ -115,8 +115,9 @@ sudo flatpak install -y com.usebruno.Bruno;
 
 # docker
 sudo dnf install -y dnf-plugins-core;
-sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo;
+sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo;
 sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
+sudo systemctl enable --now docker;
 
 ######################################################
 # PROGRAMMING LANGUAGES - PYTHON
