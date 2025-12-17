d67b1e7d1e6053af426831edcb5f0b2766bba080
Thiago Lages de Alencar <thiagola92@gmail.com>
2025-12-17 20:57:26 -0300

    Refactor install.bat for improved clarity and updates
    Updated comments for clarity and organization, added new commands for software installation and configuration, and removed deprecated commands.

--- windows/install.bat
+++ windows/install.bat
@@ -1,102 +1,260 @@
-:: TODO: Run commands one by one to check if they are working.
-:: TODO: Split commands in sections
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: REFERENCES
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-:: To run terminal as admin:
-:: - Win + X
-:: - Select "Terminal (Admin)"
+:: to run terminal as admin:
+:: - win + X
+:: - select "Terminal (Admin)"
 
-:: If the installation doesn't add the environment variable, remember to do manually
-:: because powershell and prompt truncate the replacement at 1024 characters
-:: (and don't use the command of one into the other, you will lose your $Path and will need to reinstall everything).
-
-:: Show hidden files
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
-
-:: Show files extensions
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
-
-:: Hide recents open files in explorer
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v ShowRecent /t REG_DWORD /d 0 /f
-
-:: Hide recommendation files in explorer?
-:: reg add "HKCU\Software\Policies\Microsoft\Windows\Explorer" /v HideRecommendedSection /t REG_DWORD /d 1 /f
-
-:: Disable recent opened items in Start, Jump Lists, File Explorer
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Start_TrackDocs /t REG_DWORD /d 0 /f
-
-:: Disable recently added apps in Start
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Start_TrackProgs /t REG_DWORD /d 0 /f
-
-:: Mouse sensitivity
-reg add "HKCU\Control Panel\Mouse" /v MouseSensitivity /t REG_SZ /d 5 /f
-
-:: Mouse acceleration
-reg add "HKCU\Control Panel\Mouse" /v MouseSpeed /t REG_SZ /d 0 /f
-
-:: Disable Widgets (internal name Dashboard)
-reg add "HKLM\Software\Policies\Microsoft\Dsh" /v AllowNewsAndInterests /t REG_DWORD /d 0 /f
-
-:: Disable Sticky Keys (pressing shift key 5x)
-reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v Flags /t REG_SZ /d 506 /f
-
-:: Hide Searchbox
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 0 /f
-
-:: Disable turning off screen.
-powercfg /change monitor-timeout-ac 0
-
-:: Disable sleep.
-powercfg /change standby-timeout-ac 0
-
-:: Remove softwares
-C:\Windows\System32\OneDriveSetup.exe /uninstall
-
-:: Install softwares
-winget install Brave.Brave
-winget install Git.Git
-winget install Zoom.Zoom
-winget install Discord.Discord
-winget install Microsoft.VisualStudioCode
-winget install Valve.Steam
-winget install Giorgiotani.Peazip
-winget install beekeeper-studio.beekeeper-studio
-winget install zyedidia.micro
-winget install DelugeTeam.Deluge
-winget install Spotify.Spotify
-winget install GIMP.GIMP.3
-winget install Inkscape.Inkscape
-winget install VideoLAN.VLC
-winget install OBSProject.OBSStudio
-winget install Meltytech.Shotcut
-winget install Bruno.Bruno
-winget install Docker.DockerCLI
-winget install Docker.DockerCompose
-winget install Python.Python.3.13
-winget install OpenJS.NodeJS
-winget install DenoLand.Deno
-winget install Rustlang.Rustup
-winget install GoLang.Go
-winget install RARLab.WinRar
-winget install Microsoft.VisualStudio.2022.Community
-winget install nushell
-winget install LLVM.LLVM :: Add C:\Program Files\LLVM\bin to path
-
-:: Stop startup
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v Steam /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v Discord /f
-
-:: Git config
-git config --global init.defaultBranch main
-git config --global user.name thiagola92
-git config --global user.email thiagola92@gmail.com
-git config --global core.editor micro
-git config --global credential.helper store
-git config --global pull.rebase false
-git config --global alias.graph 'log --graph'
-git config --global alias.clone-all 'clone --recurse-submodules'
-git config --global alias.pull-all 'pull --recurse-submodules'
-git config --global alias.clone-blobless 'clone --filter=blob:none'
-git config --global alias.clone-blobless-all 'clone --filter=blob:none --recurse-submodules'
+:: if the installation doesn't add the environment variable, remember to do manually
+:: because powershell and prompt truncate the replacement at 1024 characters.
+:: do not use command for setting environment variable from powershell into prompt,
+:: it doesn't replace variables when doing something like $examples.
 
 :: TODO: learn to add things to open fast
 :: Win + R and write "shell:sendto", these will appear in "Send To >"
+
+:: the following example show how to stop an application from opening at start up:
+:: reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v Discord /f
+
+:: how to discover new data to change?
+:: - creates a folder
+:: - "git init" inside the folder
+:: - export registry editor in the folder
+:: - "git add -A"
+:: - "git commit -m example"
+:: - change settings
+:: - export registry editor in the folder
+:: - use some tool that let you check the git diff
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: MANUAL
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: not a secret
+set USERNAME=thiagola92
+set EMAIL=thiagola92@gmail.com
+
+:: use the following commands to discover your SID (Security Identifier) and set it.
+whoami /user
+set SID="REPLACEWITHYOURSID"
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: OS SETTINGS 1
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: show hidden files
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
+
+:: show files extensions
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
+
+:: hide recents open files in explorer
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v ShowRecent /t REG_DWORD /d 0 /f
+
+:: disable recent opened items in Start, Jump Lists, File Explorer
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Start_TrackDocs /t REG_DWORD /d 0 /f
+
+:: disable recently added apps
+reg add "HKEY_USERS\%SID%\Software\Microsoft\Windows\CurrentVersion\Start" /v ShowRecentList /t REG_DWORD /d 0 /f
+
+:: mouse sensitivity
+reg add "HKCU\Control Panel\Mouse" /v MouseSensitivity /t REG_SZ /d 5 /f
+
+:: mouse acceleration
+reg add "HKCU\Control Panel\Mouse" /v MouseSpeed /t REG_SZ /d 0 /f
+
+:: disable Widgets (internal name Dashboard)
+reg add "HKLM\Software\Policies\Microsoft\Dsh" /v AllowNewsAndInterests /t REG_DWORD /d 0 /f
+
+:: disable Sticky Keys (pressing shift key 5x)
+reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v Flags /t REG_SZ /d 506 /f
+
+:: hide Searchbox
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 0 /f
+
+:: disable turning off screen.
+powercfg /change monitor-timeout-ac 0
+
+:: disable sleep.
+powercfg /change standby-timeout-ac 0
+
+:: remove softwares
+C:\Windows\System32\OneDriveSetup.exe /uninstall
+
+:: remove microsoft edge from desktop
+del "C:\Users\Public\Desktop\Microsoft Edge.lnk"
+
+:: switch wallpaper (TODO: check if rundll32.exe works or if it will only change after restarting)
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallPaper /t REG_SZ /d C:\windows\web\wallpaper\themea\img20.jpg /f
+rundll32.exe user32.dll, UpdatePerUserSystemParameters 1, True
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: PROGRAMMING TOOLS
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: version control
+winget install Git.Git
+git config --global init.defaultBranch main
+git config --global user.name %USERNAME%
+git config --global user.email %EMAIL%
+git config --global core.editor micro
+git config --global credential.helper store
+git config --global pull.rebase false
+git config --global alias.graph "log --graph"
+git config --global alias.clone-all "clone --recurse-submodules"
+git config --global alias.pull-all "pull --recurse-submodules"
+git config --global alias.clone-blobless "clone --filter=blob:none"
+git config --global alias.clone-blobless-all "clone --filter=blob:none --recurse-submodules"
+
+:: text editor
+winget install Microsoft.VisualStudioCode
+
+:: ide
+winget install Microsoft.VisualStudio.2022.Community
+
+:: sql database gui
+winget install beekeeper-studio.beekeeper-studio
+del "%userprofile%\Desktop\Beekeeper Studio.lnk"
+
+:: web api client
+winget install Bruno.Bruno
+del "%userprofile%\Desktop\Bruno.lnk"
+
+:: container
+winget install Docker.DockerCLI
+winget install Docker.DockerCompose
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: PROGRAMMING LANGUAGES - PYTHON
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: runtime
+winget install Python.Python.3.13
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: PROGRAMMING LANGUAGES - JAVASCRIPT
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: all-in-one
+winget install OpenJS.NodeJS
+
+:: all-in-one
+winget install DenoLand.Deno
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: PROGRAMMING LANGUAGES - RUST
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: all-in-one
+winget install Rustlang.Rustup
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: PROGRAMMING LANGUAGES - GO
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: all-in-one
+winget install GoLang.Go
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: PROGRAMMING LANGUAGES - C
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: all-in-one
+:: NOTE: Add "C:\Program Files\LLVM\bin" to path later
+winget install LLVM.LLVM
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: TERMINAL TOOLS
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: text editor
+winget install zyedidia.micro
+
+:: shell
+winget install nushell
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: GENERIC TOOLS
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: browser
+winget install Brave.Brave
+del "%userprofile%\Desktop\Brave.lnk"
+
+:: voice chat
+winget install Discord.Discord
+del "%userprofile%\Desktop\Discord.lnk"
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v Discord /f
+
+:: torrent
+winget install DelugeTeam.Deluge
+
+:: game
+winget install Valve.Steam
+del "C:\Users\Public\Desktop\Steam.lnk"
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v Steam /f
+
+:: zip
+winget install RARLab.WinRar
+winget install Giorgiotani.Peazip
+del "%userprofile%\Desktop\PeaZip.lnk"
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: AUDIO TOOLS
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: music player (can't be done as admin)
+winget install Spotify.Spotify
+del "%userprofile%\Desktop\Spotify.lnk"
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: IMAGE TOOLS
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: image editor
+winget install GIMP.GIMP.3
+del "%userprofile%\Desktop\GIMP 3.0.6-1.lnk"
+
+:: image draw
+winget install Inkscape.Inkscape
+del "C:\Users\Public\Desktop\Inkscape.lnk"
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: VIDEO TOOLS
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: video player
+winget install VideoLAN.VLC
+del "C:\Users\Public\Desktop\VLC media player.lnk"
+
+:: video editor
+winget install Meltytech.Shotcut
+
+:: video recorder
+winget install OBSProject.OBSStudio
+del "C:\Users\Public\Desktop\OBS Studio.lnk"
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: IN TESTING
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: EXPLORER RECOMMENDATION
+
+:: hide recommendation files in explorer? (TODO: check if work, this settings even exist?)
+:: reg add "HKCU\Software\Policies\Microsoft\Windows\Explorer" /v HideRecommendedSection /t REG_DWORD /d 1 /f
+
+:: Doesn't disable recently added apps, but does it help? Stopping tracking needs to be turned off?
+:: reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Start_TrackProgs /t REG_DWORD /d 0 /f
+
+:: Changing any of does settings "value" to 1 helps?
+:: Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PolicyManager\default\Start
+:: 	HideFrequentlyUsedApps
+:: 	HideRecentlyAddedApps
+::	HideRecommendedPersonalizedSites
+:: 	HideRecommendedSection
+::	ShowOrHideMostUsedApps
+
+:: COPILOT
+
+:: TODO: https://windowsforum.com/threads/remove-or-disable-windows-copilot-in-windows-11-step-by-step-guide.390362/
