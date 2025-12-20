0df1394c7e9077d923eb7ccd235fa3bf2e3ac662
Thiago Lages de Alencar <thiagola92@gmail.com>
2025-12-20 06:35:42 -0300

    Refactor backup_bookmarks.sh

--- operating systems/fedora/backup_bookmarks.sh
+++ operating systems/fedora/backup_bookmarks.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+. ~/Crons/env.sh
+
 cd /tmp
 git clone https://github.com/thiagola92/bookmarks.git tmp_repo
+
 cd tmp_repo
-cp ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks Bookmarks
-git commit -am "Backup"
+cp ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks ./Bookmarks
+git add -A
+git commit --message="Backup"
 git push
+
 cd ..
 rm -rf tmp_repo
