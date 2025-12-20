fb3d2a592a60e05bb94f9c9a122a28010e08f778
Thiago Lages de Alencar <thiagola92@gmail.com>
2025-12-20 06:38:00 -0300

    Add comments

--- operating systems/fedora/backup_bookmarks.sh
+++ operating systems/fedora/backup_bookmarks.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# load ssh key path
 . ~/Crons/env.sh
 
+# backup
 cd /tmp
 git clone https://github.com/thiagola92/bookmarks.git tmp_repo
-
 cd tmp_repo
 cp ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks ./Bookmarks
 git add -A
 git commit --message="Backup"
 git push
 
+# clean up
 cd ..
 rm -rf tmp_repo
